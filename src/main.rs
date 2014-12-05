89196a5ee33608706130c2172f6790d3ab51237d
Jon Haddad <jon@jonhaddad.com>
2014-12-05 01:44:19 -0800

    socket server starting up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::collections::hash_map::HashMap;
+use std::io::{TcpListener, TcpStream};
+use std::io::{Acceptor, Listener};
+
 
 struct CacheManager {
     data: HashMap<String, String>   
@@ -6,23 +9,24 @@ struct CacheManager {
 }
 
 impl CacheManager {
+
     fn new() -> CacheManager {
         let mut map: HashMap<String, String> = HashMap::new();
         CacheManager{data:map}
     }
     
 
-    fn _put(&mut self, key:String, val:String) {
+    fn put(&mut self, key:String, val:String) {
         self.data.insert(key, val);
     }
 
-    fn _get(&self, key:String) -> Option<&String> {
+    fn get(&self, key:String) -> Option<&String> {
         self.data.get(&key)
     }
 }
 
 #[test]
-fn simple_cache_test() {
+fn direct_cache_test() {
     let mut cm = CacheManager::new();
     cm._put("test".to_string(), "value".to_string());
     let result = cm._get("test".to_string());
@@ -37,6 +41,27 @@ fn main() {
     let mut cm = CacheManager::new();
 
     println!("starting up socket server");
+
+    let listener = TcpListener::bind("127.0.0.1:7777");
+    
+    let mut acceptor = listener.listen();
+
+    fn handle_client(mut stream: TcpStream) {
+        // ...
+    }
+    // accept connections and process them, spawning a new tasks for each one
+    for stream in acceptor.incoming() {
+        match stream {
+            Err(e) => { /* connection failed */ }
+            Ok(stream) => spawn(proc() {
+                // connection succeeded
+                handle_client(stream)
+            })
+        }
+    }
+
+    // close the socket server
+    drop(acceptor);
 
     println!("done");
 }
