2c2868d9faf9816bd7b4bfce978329b4102de84b
Jon Haddad <jon@jonhaddad.com>
2014-12-21 14:01:26 -0800

    lend out the keys instead of moving them

--- src/cache.rs
+++ src/cache.rs
@@ -17,10 +17,14 @@ impl CacheManager {
         self.data.insert(key, val);
     }
 
+    pub fn set(&mut self, key:&String, val:String) {
+        self.data.insert(key.clone(), val);
+    }
+
     pub fn get(&self, key:String) -> Option<&String> {
         self.data.get(&key)
     }
-    pub fn increment(&self, key:String) -> Option<i64> {
+    pub fn increment(&self, key:&String, value:i64) -> Option<i64> {
         return Some(0i64);
     }
 }
@@ -35,4 +39,16 @@ fn direct_cache_test() {
 #[test]
 fn test_cache_manager_get() {
     let mut c = CacheManager::new();
+}
+
+#[test]
+fn increment_test() {
+    let mut c = CacheManager::new();
+    let s = "test".to_string();
+
+    c.set(&s, "0".to_string());
+    c.increment(&s, 1i64);
+
+    let result = c.get(s).unwrap();
+    //assert_eq!(result, "1".to_string());
 }
