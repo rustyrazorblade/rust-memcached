ec06af646cb670c1308b23ba28569f314773c96c
Jon Haddad <jon@jonhaddad.com>
2014-12-20 17:51:08 -0800

    testing variety of keys now

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ from time import time
 start = time()
 num = 1000
 for x in range(num):
-    c.set("test", str(x), noreply=False)
-    val = c.get('test')
+    c.set(str(x), str(x), noreply=False)
+    val = c.get(str(x))
     assert val == str(x), str(x)
 
 print num / (time() - start)
